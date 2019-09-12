1e4cfc700577f19dd6f052e83fdd7547a78ef7a5
hellozjf <908686171@qq.com>
2019-09-12 20:17:12 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import { Button, Layout, Menu, Icon } from 'ant-design-vue';
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.use(Button);
+Vue.use(Layout);
+Vue.use(Menu);
+Vue.use(Icon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// 参考：https://blog.csdn.net/weixin_41652495/article/details/83626651
+module.exports = {
+  css: {
+    loaderOptions: { // 向 CSS 相关的 loader 传递选项
+      less: {
+        javascriptEnabled: true
+      }
+    }
+  }
+}
